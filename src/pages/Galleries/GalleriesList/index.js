29fb8354de62129e31e9130bab785ae589b4115d
Fabio Montone <montone@gmail.com>
2020-06-04 20:35:55 -0300

    Fix: Lists Exports

---
+++ src/pages/Galleries/GalleriesList/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+function GalleriesList() {
+  return <div />;
+}
+
+export default GalleriesList;
---
+++ src/pages/Users/UsersList/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+function UsersList() {
+  return <div />;
+}
+
+export default UsersList;
