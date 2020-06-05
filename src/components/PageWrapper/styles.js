d90728a70fd7d9eb17772d50bd79e676ad3af8f8
Fabio Montone <montone@gmail.com>
2020-06-05 15:15:20 -0300

    Fix: PageWrapper

--- src/components/PageWrapper/styles.js
+++ src/components/PageWrapper/styles.js
@@ -3,8 +3,6 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
   width: 100%;
   height: 100%;
-  min-height: 100vh;
-  min-width: 100vw;
   display: flex;
   flex-direction: column;
   align-items: center;
