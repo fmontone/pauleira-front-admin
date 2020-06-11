a208e084293e54553fed46eb06d075d002b8088a
Fabio Montone <montone@gmail.com>
2020-06-11 13:58:40 -0300

    Fix: Search Form

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -2,17 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { MdSearch } from 'react-icons/md';
-import { Input } from '~/components/Form';
 
-import { Form, ButtonSubmit } from './styles';
+import { Form, InputField, ButtonSubmit } from './styles';
 
 import colors from '~/styles/colors';
 
 function SearchForm({ placeholder, ...rest }) {
   return (
     <Form {...rest}>
-      <Input placeholder={placeholder} />
-      <ButtonSubmit>
+      <InputField type="search" name="search" placeholder={placeholder} />
+      <ButtonSubmit type="submit">
         <MdSearch color={colors.greyLight} size="24" />
       </ButtonSubmit>
     </Form>
@@ -22,6 +21,7 @@ function SearchForm({ placeholder, ...rest }) {
 SearchForm.propTypes = {
   placeholder: PropTypes.string,
 };
+
 SearchForm.defaultProps = {
   placeholder: '',
 };
--- src/components/SearchForm/styles.js
+++ src/components/SearchForm/styles.js
@@ -1,7 +1,25 @@
 import styled from 'styled-components';
 
+import colors from '~/styles/colors';
+
 export const Form = styled.form`
   position: relative;
+`;
+
+export const InputField = styled.input`
+  width: 100%;
+  height: 32px;
+  padding: 0 8px;
+  position: relative;
+  font-size: 16px;
+  color: ${colors.black};
+  border: 1px solid ${colors.greyLight};
+  border-radius: 6px;
+  background-color: ${colors.white};
+
+  ::placeholder {
+    color: ${colors.greyLight};
+  }
 `;
 
 export const ButtonSubmit = styled.button`
@@ -16,4 +34,10 @@ export const ButtonSubmit = styled.button`
   display: flex;
   align-items: center;
   justify-content: center;
+
+  :active {
+    transform: translate(0, 1px);
+    height: 29px;
+    width: 31px;
+  }
 `;
