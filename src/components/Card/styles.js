b60f6f65e25372955ff54032a5af08b3f1d86f56
Fabio Montone <montone@gmail.com>
2020-10-08 10:04:45 -0300

    Create: Server + Deploy

--- src/components/Card/styles.js
+++ src/components/Card/styles.js
@@ -8,5 +8,6 @@ export const Container = styled.div`
 
   border-radius: 6px;
   background: ${colors.white};
-  box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.1);
+  box-shadow: box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  /* box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.1); */
 `;
