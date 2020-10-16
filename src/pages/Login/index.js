78bcc823f410d7d9081979962f5a3dd45be32e7b
Fabio Montone <montone@gmail.com>
2020-10-16 14:58:03 -0300

    Fix: Users State

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -40,7 +40,6 @@ function Login() {
           <img src={PauleiraLogo} alt="Pauleira Guitars Logo" />
         </Logo>
         <Title>Admin Login</Title>
-        <p>{process.env.PUBLIC_URL}</p>
       </PageHeader>
 
       <Form onSubmit={handleSubmit(handleDataSubmit)}>
